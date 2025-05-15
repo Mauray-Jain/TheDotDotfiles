0441de601626911c230c8d7ff9b39750c531ce7c
Mauray-Jain <maurayjain@gmail.com>
2025-05-16 00:19:16 +0530

    Demn suckless is a commitment

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 ~/.fehbg &
-xrandr --dpi 120 &
 picom &
 xss-lock -- slock &
+slstatus &
+nm-applet &
+dunst &
--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -4,6 +4,11 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 15;       /* gaps between windows */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font Mono:size=10", "monospace:size=10" };
@@ -91,6 +96,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_b,                     togglebar,      {0} },
 	{ MODKEY,                       XK_j,                     focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,                     focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,                     rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,                     rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_i,                     incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,                     incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,                     setmfact,       {.f = -0.05} },
